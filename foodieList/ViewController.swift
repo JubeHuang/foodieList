c13eda66e3a5fe0da87787c6569cb199965df551
Jube Huang <108262303+JubeHuang@users.noreply.github.com>
2023-01-05 19:23:46 +0800

    Initial Commit

---
+++ foodieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodieList
+//
+//  Created by Jube on 2023/1/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
